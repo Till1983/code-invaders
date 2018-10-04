f30eb64f03632efe526b9bdaa6ad97c97432fab5
Esteban Ortiz <42191827+estebanortiz55@users.noreply.github.com>
2018-10-04 13:50:33 +0200

    Bunkers now have their game image

--- blocks.js
+++ blocks.js
@@ -1,13 +1,10 @@
 function Bunker(x, y) {
-    this.x = x;
-    this.y = y;
-    this.diameter = 38;
-    // insert here: this.sprite(foldername/filename.filetype)
+  this.x = x;
+  this.y = y;
+  this.diameter = 38;
+  this.sprite = loadImage("bunkers/BunkersTop1-.png")
 
-    this.show = function() {
-        fill(240, 0, 240);
-        rectMode(CENTER);
-        rect(this.x, height - 80, this.diameter, this.diameter); // insert 'this.sprite' before 'this.x',
-        														                            //replace 'rect' with 'image', and delete line 8 and line 9
-    }
+  this.show = function() {
+    image(this.sprite, this.x, height - 120, this.diameter, this.diameter);
+  }
 }
